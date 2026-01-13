5412f9278d906957a7629b3d2b16cfbe00830719
JOSnow985 <jaden.olvera@students.snow.edu>
2026-01-12 22:10:57 -0700

    Step 1: Take user input for message and key, not encrypted yet

---
+++ Program.cs
@@ -0,0 +1,8 @@
+﻿Console.Clear();
+Console.WriteLine("Let's encrypt a message using the Vigenere method!");
+Console.WriteLine("Please give us a message to encrypt:");
+string userMessage = Console.ReadLine();
+Console.WriteLine("Please enter the encryption key you want to use:");
+string userKey = Console.ReadLine();
+Console.WriteLine($"Your message: {userMessage}");
+Console.WriteLine($"Your encryption key: {userKey}");
